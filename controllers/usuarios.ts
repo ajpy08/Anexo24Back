63f24e029f4c7db36e3779408a353474956c5dee
I.S.C. Angel Javier Puc Yamá <angelpuc08@gmail.com>
2021-04-13 15:27:45 -0500

    postUsuario and putUsuario done!

--- controllers/usuarios.ts
+++ controllers/usuarios.ts
@@ -11,7 +11,7 @@ export const getUsuarios = async (req: Request, res: Response) => {
 }
 
 export const getUsuario = async (req: Request, res: Response) => {
-    const {id} = req.params;
+    const { id } = req.params;
 
     const usuario = await Usuario.findByPk(id);
 
@@ -26,28 +26,64 @@ export const getUsuario = async (req: Request, res: Response) => {
     })
 }
 
-export const postUsuario = (req: Request, res: Response) => {
-    const {body} = req;
+export const postUsuario = async (req: Request, res: Response) => {
+    const { body } = req;
 
-    res.json({
-        msg: 'postUsuario',
-        body
-    })
+    try {
+        const existeEmail = await Usuario.findOne({
+            where: {
+                email: body.email
+            }
+        });
+
+        if (existeEmail) {
+            return res.status(400).json({
+                msg: `Ya existe un usuario con el email ${body.email}`
+            });
+        }
+
+        const usuario = Usuario.build(body);
+        await usuario.save();
+
+        res.json({
+            usuario
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: 'Hable con el administrador'
+        })
+    }
 }
 
-export const putUsuario = (req: Request, res: Response) => {
-    const {id} = req.params;
-    const {body} = req;
+export const putUsuario = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { body } = req;
 
-    res.json({
-        id,
-        msg: 'putUsuario',
-        body
-    })
+    try {
+        const usuario = await Usuario.findByPk(id);
+        if(!usuario){
+            return res.status(404).json({
+                msg: `No existe un usuario con el id ${id}`
+            })
+        }
+
+        await usuario.update(body);
+
+        res.json({
+            usuario
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: 'Hable con el administrador'
+        });
+    }
 }
 
 export const deleteUsuario = (req: Request, res: Response) => {
-    const {id} = req.params;
+    const { id } = req.params;
 
     res.json({
         id,
