4108537f8a8459d47e7e998fee55d440941668b9
I.S.C. Angel Javier Puc Yamá <angelpuc08@gmail.com>
2021-05-26 17:52:50 -0500

    tmp2 26-05-21

--- controllers/empresa.ts
+++ controllers/empresa.ts
@@ -2,135 +2,76 @@ import { Empresa } from './../models/empresa';
 import { Request, Response } from "express";
 import { QueryTypes } from "sequelize";
 import db from "../db/connection";
+import empresaController from './../controllers/empresa';
 
 export = {
     getEmpresas: async (req: Request, res: Response) => {
-        try {
-            const empresas = await db.query('SELECT * from empresas', {
-                type: QueryTypes.SELECT
-            });
+        // try {
+        return db.query('SELECT * from empresas', {
+            type: QueryTypes.SELECT,
+            model: Empresa
+        });
 
-            res.status(200).json({
-                empresas,
-                total: empresas.length
-            });
-        } catch (error) {
-            res.status(500).json({
-                msg: `Ocurrio un error ${error}`
-            });
-        }
+        // res.status(200).json({
+        //     empresas,
+        //     total: empresas.length
+        // });
+        // } catch (error) {
+        //     res.status(500).json({
+        //         msg: `Ocurrio un error ${error}`
+        //     });
+        // }
     },
-    getEmpresa: async (req: Request, res: Response) => {
+    getEmpresa: (req: Request, res: Response) => {
         const { id } = req.params;
 
-        try {
+        // return Empresa.findByPk(id);
 
-            // const empresa = await Empresa.findByPk(id);
-
-            // const empresa = await db.query(`SELECT * from empresas WHERE id = ${id}` , {
-            //     // A function (or false) for logging your queries
-            //     // Will get called for every SQL query that gets sent
-            //     // to the server.
-            //     // logging: console.log,
-            //     logging: false,
-
-            //     // Set this to true if you don't have a model definition for your query.
-            //     // raw: false,
-
-            //     // The type of query you are executing. The query type affects how results are formatted before they are passed back.
-            //     type: QueryTypes.SELECT
-            // });
-
-            const empresa = await db.query(
-                'SELECT * FROM empresas WHERE empresaId = :id',
-                {
-                    replacements: { id: 1 },
-                    type: QueryTypes.SELECT
-                }
-            );
-
-            if (!empresa) {
-                res.status(404).json({
-                    msg: `No existe una empresa con el id ${id}`
-                });
+        return db.query(
+            'SELECT * FROM empresas WHERE empresaId = :id',
+            {
+                replacements: { id },
+                type: QueryTypes.SELECT
             }
-
-            res.status(200).json({
-                empresa
-            });
-        } catch (error) {
-            res.status(500).json({
-                msg: `Ocurrio un error ${error}`
-            });
-        }
+        );
     },
-    getEmpresasByUser: async (req: Request, res: Response) => {
+    getEmpresasByUser: (req: Request, res: Response) => {
         const { userId } = req.params;
-        try {
-            const empresas = await db.query(
-                'SELECT empresas.* FROM empresas ' +
-                'INNER JOIN user_empresas USING (empresaId) ' +
-                'INNER JOIN users USING (userId) ' +
-                'WHERE users.userId = :userId',
-                {
-                    replacements: { userId },
-                    type: QueryTypes.SELECT,
-                    model: Empresa
-                }
-            );
 
-            if (!empresas) {
-                res.status(404).json({
-                    msg: `No existe una empresa para el usuario con id ${userId}`
-                });
+        return db.query(
+            'SELECT empresas.* FROM empresas ' +
+            'INNER JOIN user_empresas USING (empresaId) ' +
+            'INNER JOIN users USING (userId) ' +
+            'WHERE users.userId = :userId',
+            {
+                replacements: { userId },
+                type: QueryTypes.SELECT,
+                model: Empresa
             }
-
-
-            return empresas;
-            // res.status(200).json({
-            //     empresas,
-            //     total: empresas.length
-            // });
-        } catch (error) {
-            res.status(500).json({
-                msg: `Ocurrio un error ${error}`
-            });
-        }
+        );
     },
-    postEmpresa: async (req: Request, res: Response) => {
+    postEmpresa: (req: Request, res: Response) => {
         const { body } = req;
-        try {
-            const existRFC = await Empresa.findOne({
-                where: {
-                    rfc: body.rfc
-                }
-            });
 
-            if (existRFC) {
-                return res.status(400).json({
-                    msg: `Ya existe una empresa con el rfc ${body.rfc}`
-                });
+        const empresa = Empresa.build(body);
+        return empresa.save();
+
+        // return db.query(
+        //     `INSERT INTO empresas (rfc, nombre) VALUES ("${empresa.rfc}", "${empresa.nombre}");`,
+        //     {
+        //         type: QueryTypes.INSERT
+        //     }
+        // ).then((result) => {
+        //     empresa.id = result[0];
+        // });
+    },
+    verificaRFC: (req: Request, res: Response) => {
+        const { body } = req;
+        return Empresa.findOne({
+            where: {
+                rfc: body.rfc
             }
-
-            const empresa = Empresa.build(body);
-            await empresa.save();
-
-            // await db.query(
-            //     `INSERT INTO empresas (rfc, nombre) VALUES ("${empresa.rfc}", "${empresa.nombre}");`,
-            //     {
-            //         type: QueryTypes.INSERT
-            //     }
-            // ).then((result) => {
-            //     empresa.id = result[0];
-            res.status(200).json({
-                empresa
-            });
-            // });
-        } catch (error) {
-            res.status(500).json({
-                msg: `Ocurrio un error ${error}`
-            });
-        }
+        });
     },
     putEmpresa: async (req: Request, res: Response) => {
         const { id } = req.params;
--- routes/empresa.ts
+++ routes/empresa.ts
@@ -1,16 +1,125 @@
+import { postUser } from './../controllers/users';
 import empresaController from './../controllers/empresa';
 import { Router } from 'express';
 import { verifyToken } from '../controllers/auth';
 
 const router = Router();
 
-router.get('/', verifyToken, empresaController.getEmpresas);
-router.get('/:id', verifyToken, empresaController.getEmpresa);
-router.post('/', verifyToken, empresaController.postEmpresa);
-router.put('/:id', verifyToken, empresaController.putEmpresa);
-router.delete('/:id', verifyToken, empresaController.deleteEmpresa);
-router.get('/user/:userId', verifyToken, async (req, res) => {
-    await empresaController.getEmpresasByUser(req, res)
+router.get('/', verifyToken, (req, res) => {
+    empresaController.getEmpresas(req, res).then(empresas => {
+        if (empresas) {
+            res.status(200).json({
+                empresas,
+                total: empresas.length
+            });
+        } else {
+            res.status(400).json({
+                msg: `No existen empresas`,
+            });
+        }
+    }).catch(error => {
+        res.status(500).json({
+            msg: `Ocurrio un error ${error}`
+        });
+    });
 });
+
+router.get('/:id', verifyToken, (req, res) => {
+    empresaController.getEmpresa(req, res).then(empresa => {
+        if (empresa) {
+            res.status(200).json({
+                empresa
+            });
+        } else {
+            res.status(400).json({
+                msg: `No existe una empresa con el id ${req.params.id}`
+            });
+        }
+    });
+});
+
+router.get('/user/:userId', verifyToken, (req, res) => {
+    empresaController.getEmpresasByUser(req, res).then(empresas => {
+        if (empresas) {
+            res.status(200).json({
+                empresas,
+                total: empresas.length
+            });
+        } else {
+            res.status(400).json({
+                msg: `No existen empresas`,
+            });
+        }
+    }).catch(error => {
+        res.status(500).json({
+            msg: `Ocurrio un error ${error}`
+        });
+    });
+});
+
+router.post('/', verifyToken, async (req, res) => {
+    const { body } = req;
+
+    const existeRFC = await empresaController.verificaRFC(req, res);
+
+    if (existeRFC) {
+        return res.status(400).json({
+            msg: `Ya existe una empresa con el rfc ${body.rfc}`
+        });
+    }
+
+    empresaController.postEmpresa(req, res).then(empresa => {
+        res.status(200).json({
+            empresa
+        });
+    }).catch(error => {
+        res.status(500).json({
+            msg: `Ocurrio un error ${error}`
+        });
+    });
+});
+
+router.get('/verificaRFC', verifyToken, (req, res) => {
+    empresaController.verificaRFC(req, res).then(empresa => {
+        if (empresa) {
+            res.status(200).json({
+                empresa
+            });
+        } else {
+            res.status(400).json({
+                msg: `No existe empresa con RFC ${req.body.rfc}`,
+            });
+        }
+    }).catch(error => {
+        res.status(500).json({
+            msg: `Ocurrio un error ${error}`
+        });
+    });
+});
+
+router.put('/:id', verifyToken, async (req, res) => {
+    const { id } = req.params;
+    const { body } = req;
+
+    // const existeEmpresa = await empresaController.getEmpresa(req, res);
+
+    // if (!existeEmpresa) {
+    //     return res.status(400).json({
+    //         msg: `No existe una empresa con el id ${id}`
+    //     });
+    // }
+
+    empresaController.putEmpresa(req, res).then(empresa => {
+        res.status(200).json({
+            empresa
+        });
+    }).catch(error => {
+        res.status(500).json({
+            msg: `Ocurrio un error ${error}`
+        });
+    });
+});
+
+router.delete('/:id', verifyToken, empresaController.deleteEmpresa);
 
 export default router;
